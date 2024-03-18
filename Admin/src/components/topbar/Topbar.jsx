114087adefbdeac2dcabdff440b243c9232334ce
longnhat202142 <longnhat03082002@gmail.com>
2024-03-18 10:03:26 +0700

    Background login + Logout

--- Admin/src/components/topbar/Topbar.jsx
+++ Admin/src/components/topbar/Topbar.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "./topbar.css";
 import { NotificationsNone, Language, Settings } from "@material-ui/icons";
-
+import { AuthContext } from "../../context/authContext/authContext.js";
+import { useContext } from "react";
+import { logout } from "../../context/authContext/authAction.js";
 export default function Topbar() {
+  const { dispatch } = useContext(AuthContext);
   return (
     <div className="topbar">
       <div className="topbarWrapper">
         <div className="topLeft">
-          <span className="logo">lamaadmin</span>
+          <span className="logo">Trang chủ Admin</span>
         </div>
         <div className="topRight">
           <div className="topbarIconContainer">
@@ -21,7 +24,17 @@ export default function Topbar() {
           <div className="topbarIconContainer">
             <Settings />
           </div>
-          <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" />
+          <img
+            src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+            className="topAvatar"
+          />
+
+          <div className="topbarIconContainer">
+            <span className="topbarLogOut" onClick={() => dispatch(logout())}>
+              Đăng xuất
+            </span>
+          </div>
         </div>
       </div>
     </div>
--- Admin/src/pages/login/Login.jsx
+++ Admin/src/pages/login/Login.jsx
@@ -1,39 +1,53 @@
 import React, { useContext, useState } from "react";
 import { login } from "../../context/authContext/apiCalls";
 import { AuthContext } from "../../context/authContext/authContext";
+import PersonIcon from "@mui/icons-material/Person";
+import PasswordOutlinedIcon from "@mui/icons-material/Password";
+import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 import "./Login.css";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const { isFetching, dispatch, user } = useContext(AuthContext);
+  const { isFetching, dispatch } = useContext(AuthContext);
 
   const handleLogin = (e) => {
     login({ email, password }, dispatch);
-    if (user) {
-      console.log(user);
-    }
   };
-
+  const [showPass, setShowPass] = useState(false);
+  const togglePass = () => {
+    setShowPass(!showPass);
+  };
   return (
-    <div className="login">
-      <form className="loginForm">
-        <input
-          type="text"
-          placeholder="email"
-          className="loginInput"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Mật khẩu"
-          className="loginInput"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+    <div className="wrapper">
+      <form className="form-login">
+        <h1 className="form-heading">Đăng nhập</h1>
+        <div className="form-group">
+          <PersonIcon className="iconLogin" />
+          <input
+            type="text"
+            placeholder="Email"
+            className="form-input"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <PasswordOutlinedIcon className="iconLogin" />
+          <input
+            type={showPass ? "text" : "password"}
+            placeholder="Mật khẩu"
+            className="form-input"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className="eye">
+            <VisibilityOutlinedIcon onClick={togglePass} />
+          </div>
+        </div>
+
         <button
-          className="loginButton"
+          className="form-submit"
           onClick={handleLogin}
           disabled={isFetching}
         >
